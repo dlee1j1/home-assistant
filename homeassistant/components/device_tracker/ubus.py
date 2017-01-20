8654ff85f082dec5f22f1a8486387b6fb9ae6240
Teemu Rytilahti <tpr@iki.fi>
2017-01-20 23:32:57 +0100

    ubus: do not do i/o on setup_scanner(), try to re-login on expired/failed sessions

--- homeassistant/components/device_tracker/ubus.py
+++ homeassistant/components/device_tracker/ubus.py
@@ -48,12 +48,14 @@ class UbusDeviceScanner(DeviceScanner):
     def __init__(self, config):
         """Initialize the scanner."""
         host = config[CONF_HOST]
-        username, password = config[CONF_USERNAME], config[CONF_PASSWORD]
+        self.username = config[CONF_USERNAME]
+        self.password = config[CONF_PASSWORD]
 
         self.parse_api_pattern = re.compile(r"(?P<param>\w*) = (?P<value>.*);")
         self.lock = threading.Lock()
         self.last_results = {}
         self.url = 'http://{}/ubus'.format(host)
+<<<<<<< HEAD
 
         self.session_id = _get_session_id(self.url, username, password)
         self.hostapd = []
@@ -122,6 +124,101 @@ class UbusDeviceScanner(DeviceScanner):
                     self.last_results.extend(result['clients'].keys())
 
             return bool(results)
+=======
+        self.session_id = None  # lazy init, will be fetched on first error
+
+
+    def login(self):
+        _LOGGER.debug("Fetching the session id..")
+        self.session_id = _get_session_id(self.url, self.username, self.password)
+        _LOGGER.debug("Got session: %s", self.session_id)
+
+    def update(self, see):
+        """Fetch clients and leases from the router."""
+        clients = self._get_devices()
+        leases = self._get_leases()
+        _LOGGER.info("Got %s clients, %s leases", len(clients), len(leases))
+
+        # Note, we may have clients who have leases expired..
+        for client in clients:
+            mac = client["mac"].replace(":", "").lower()
+            lease = [lease for lease in leases if lease["mac"] == mac]
+            if lease:
+                client["ip"] = lease[0]["ip"]
+                client["hostname"] = lease[0]["hostname"]
+            else:
+                _LOGGER.warning("Couldn't find lease for %s", client["mac"])
+                client["hostname"] = "<no lease>"
+                client["ip"] = "<no lease>"
+
+            extra_attrs = {
+                "ip": client["ip"],
+                "signal": client["signal"],
+                "noise": client["noise"]
+            }
+            # _LOGGER.info("Seen: %s (%s)", client["hostname"], client["mac"])
+            see(mac=client["mac"], host_name=client["hostname"],
+                source_type=SOURCE_TYPE_ROUTER,
+                attributes=extra_attrs)
+
+        return True
+
+    def _get_devices(self):
+        """Request all connected devices."""
+        clients = []
+
+        try:
+            ifaces = _req_json_rpc(self.url, self.session_id,
+                                   "call", "iwinfo", "devices")
+        except UbusException as ex:
+            _LOGGER.error("Unable to fetch interfaces: %s", ex)
+            self.login()  # try to renew the session
+            return clients
+
+        # _LOGGER.debug("Found %s ifaces: %s", len(ifaces), ifaces)
+        for iface in ifaces["devices"]:
+            devices = _req_json_rpc(self.url, self.session_id,
+                                    "call", "iwinfo", "assoclist",
+                                    device=iface)
+            if "results" in devices:
+                for dev in devices["results"]:
+                    # _LOGGER.debug("device: %s", dev)
+                    clients.append(dev)
+
+        # example client
+        # [{'signal': -47, 'inactive': 310,
+        # 'tx': {'mcs': 7, '40mhz': False, 'rate': 65000, 'short_gi': False},
+        # 'mac': 'F0:B4:29:XX:XX:XX',
+        # 'rx': {'mcs': 7, '40mhz': False, 'rate': 72200, 'short_gi': True},
+        # 'noise': -95}]
+        # _LOGGER.info("Found %s clients: %s", len(clients),
+        #             [client["mac"] for client in clients])
+
+        return clients
+
+    def _get_leases(self):
+        """Get all DHCP leases to obtain hostnames."""
+        leases = []
+        for ip_version in ["ipv4leases", "ipv6leases"]:
+            try:
+                lease_res = _req_json_rpc(self.url, self.session_id,
+                                          "call", "dhcp", ip_version)
+            except UbusException as ex:
+                _LOGGER.error("Unable to fetch leases: %s", ex)
+                self.login()  # try to renew the session
+                return leases
+            for network in lease_res["device"]:
+                # _LOGGER.debug("Checking network %s" % network)
+                for lease in lease_res["device"][network]["leases"]:
+                    # _LOGGER.debug("[%s] client: %s", network, lease)
+                    leases.append(lease)
+
+        # example lease
+        # {'mac': '286c07xxxxxx', 'valid': -7471,
+        # 'hostname': 'XXXXX','ip': '192.168.250.132'}
+
+        return leases
+>>>>>>> ubus: do not do i/o on setup_scanner(), try to re-login on expired/failed sessions
 
 
 def _req_json_rpc(url, session_id, rpcmethod, subsystem, method, **params):
@@ -155,3 +252,28 @@ def _get_session_id(url, username, password):
                         'session', 'login', username=username,
                         password=password)
     return res["ubus_rpc_session"]
+<<<<<<< HEAD
+=======
+
+
+def setup_scanner(hass, config, see):
+    """Setup an endpoint for the ubus logger."""
+    try:
+        _LOGGER.info("Trying to start the scanner..")
+        scanner = UbusDeviceScanner(config)
+        interval = DEFAULT_SCAN_INTERVAL
+        _LOGGER.info("Started ubustracker with interval=%s", interval)
+
+        def update(now):
+            """Update all the hosts on every interval time."""
+            scanner.update(see)
+            track_point_in_utc_time(hass, update, now + interval)
+            return True
+
+        return update(util.dt.utcnow())
+    except UbusException as ex:
+        _LOGGER.error("Got exception: %s", ex)
+        return False
+
+    return True
+>>>>>>> ubus: do not do i/o on setup_scanner(), try to re-login on expired/failed sessions
