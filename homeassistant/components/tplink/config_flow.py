09bddd5c4b4a8aa341e20959d05c2b4308cb1070
dlee1j1 <dlee1j@yahoo.comm>
2021-03-01 01:54:05 +0000

    clean up

--- homeassistant/components/tplink/config_flow.py
+++ homeassistant/components/tplink/config_flow.py
@@ -5,14 +5,11 @@ from homeassistant.helpers import config_entry_flow
 # from .common import async_get_discoverable_devices
 from .const import DOMAIN
 
-async def updater(hass):
+
+async def _updater(hass):
     return ["updater"]
 
+
 config_entry_flow.register_discovery_flow(
-    DOMAIN,
-    "TP-Link Smart Home",
-#    async_get_discoverable_devices,
-#    Discover.discover(),
-    updater,
-    config_entries.CONN_CLASS_LOCAL_POLL,
+    DOMAIN, "TP-Link Smart Home", _updater, config_entries.CONN_CLASS_LOCAL_POLL,
 )
